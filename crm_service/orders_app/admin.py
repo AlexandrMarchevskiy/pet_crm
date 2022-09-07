b2fb3b5f2a2ea854e855cbc05c9737bce70f2116
AlexandrMarchevskiy <alexander.marchevsky@yandex.ru>
2022-09-08 01:37:28 +0300

    [upd] админка, модель заявки #2

--- crm_service/orders_app/admin.py
+++ crm_service/orders_app/admin.py
@@ -3,16 +3,62 @@ from django.contrib import admin
 from .models import Order, Device, Customer, DeviceInField
 
 class DeviceAdmin(admin.ModelAdmin):
+    search_fields = ('manufacturer', 'model')
     list_display = ('id', 'manufacturer', 'model')
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'device', 'customer', 'order_description', 'created_dt', 'last_updated_dt', 'order_status')
+    # задаём методы для получения полей из связанных таблиц
+    def my_customer(self, obj):
+        return obj.device.customer_id.customer_name
+
+    def my_serial_number(self, obj):
+        return obj.device.serial_number
+
+    def my_device_model(self, obj):
+        return obj.device.analyzer_id.model
+
+    def my_device_manufacturer(self, obj):
+        return obj.device.analyzer_id.manufacturer
+
+    # задаём отображаемое название полей в админке
+    my_customer.short_description = 'Пользователь'
+    my_serial_number.short_description = 'Серийный номер'
+    my_device_model.short_description = 'Модель'
+    my_device_manufacturer.short_description = 'Производитель'
+
+    # поля для отображения
+    list_display = ('id', 'my_device_manufacturer', 'my_device_model', 'my_serial_number',
+                    'my_customer', 'order_description', 'created_dt', 'last_updated_dt', 'order_status')
+
+    # поля для поиска
+    search_fields = ('device__customer__customer_name', 'device__id', 'device__serial_number',
+                     'device__analyzer__model', 'device__analyzer__manufacturer')
+
+    # поля для того , что бы заменить выпадашку на ввод инфы
+    raw_id_fields = ('device', )
+
 
 class CustomerAdmin(admin.ModelAdmin):
+    search_fields = ('customer_name', 'customer_adress')
     list_display = ('id', 'customer_name', 'customer_adress', 'customer_city')
 
 class DeviceInFieldAdmin(admin.ModelAdmin):
-    list_display = ('id', 'serial_number', 'customer_id', 'analyzer_id', 'owner_status')
+    def my_customer(self, obj):
+        return obj.customer_id.customer_name
+
+    def my_device_model(self, obj):
+        return obj.analyzer_id.model
+
+    def my_device_manufacturer(self, obj):
+        return obj.analyzer_id.manufacturer
+
+    my_customer.short_description = 'Пользователь'
+    my_device_manufacturer.short_description = 'Производитель'
+    my_device_model.short_description = 'Модель'
+
+    search_fields = ('serial_number', )
+    raw_id_fields = ('customer_id', 'analyzer_id')
+    list_display = ('id', 'my_device_manufacturer', 'my_device_model', 'my_customer', 'serial_number', 'customer_id', 'analyzer_id', 'owner_status')
 
 admin.site.register(Order, OrderAdmin)
 admin.site.register(Device, DeviceAdmin)
--- crm_service/orders_app/models.py
+++ crm_service/orders_app/models.py
@@ -31,7 +31,7 @@ class Customer(models.Model):
     customer_city = models.TextField(verbose_name='Город')
 
     def __str__(self):
-        return self.customer_name
+        return f'{self.customer_name} по адресу {self.customer_adress}'
 
 
 class DeviceInField(models.Model):
@@ -48,15 +48,9 @@ class DeviceInField(models.Model):
     owner_status = models.TextField(verbose_name='Статус принадлежности')
 
     def __str__(self):
-        return f'{self.serial_number} {self.analyzer_id}'
+        return f'{self.analyzer_id} с/н {self.serial_number} в {self.customer_id}'
 
 
-def status_validator(order_staus):
-    if order_staus not in ['open', 'closed', 'in progress', 'need info']:
-        raise ValidationError(
-            gettext_lazy('%(order_status)s is wrong order status'),
-            params={'order_status': order_staus},
-        )
 
 
 class Order(models.Model):
@@ -67,12 +61,19 @@ class Order(models.Model):
         verbose_name = 'Заявка'
         verbose_name_plural = 'Заявки'
 
+    statuses = (('open', 'открыта'),
+                ('closed', 'закрыта'),
+                ('in progress', 'в работе'),
+                ('need info', 'нужна информация'))
+
     device = models.ForeignKey(DeviceInField, verbose_name='Оборудование', on_delete=models.RESTRICT)
-    customer = models.ForeignKey(Customer, verbose_name='Конечный пользователь', on_delete=models.RESTRICT)
     order_description = models.TextField(verbose_name='Описание')
     created_dt = models.DateTimeField(verbose_name='Создано', auto_now_add=True)
     last_updated_dt = models.DateTimeField(verbose_name='Последнее изменение', blank=True, null=True)
-    order_status = models.TextField(verbose_name='Статус заявки', validators=[status_validator])
+    order_status = models.TextField(verbose_name='Статус заявки', choices=statuses)
+
+    def __str__(self):
+        return f'Заявка №{self.id} для {self.device}'
 
     def save(self, *args, **kwargs):
         self.last_updated_dt = datetime.now()
